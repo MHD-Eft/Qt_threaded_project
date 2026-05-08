ecfdb3ed20e382a49f931b3caca3c16112cd7106
Mahdieh Eftekharian <80527537+MHD-Eft@users.noreply.github.com>
2026-05-08 12:25:38 +0330

    Create worker.cpp

---
+++ worker.cpp
@@ -0,0 +1,40 @@
+#include "worker.h"
+
+Worker::Worker(QObject *parent) : QObject(parent)
+{
+    qInfo() << this << "Constructed on " << QThread::currentThread();
+    connect(&timer,&QTimer::timeout,this,&Worker::timeout);
+    timer.setInterval(1000);
+
+}
+
+Worker::~Worker()
+{
+    qInfo() << this << "Deconstruted on "<< QThread::currentThread();
+
+}
+
+void Worker::start()
+{
+    qInfo() << this << "Started on " << QThread::currentThread();
+    timer.start();
+    loop.exec(); //processing stays here
+
+    qInfo() << this << "Loop finished on " << QThread::currentThread();
+
+    //this->deleteLater(); //may hold on this for a while!
+    delete this;
+}
+
+void Worker::stop()
+{
+    qInfo() << this << "Stopping on "<< QThread::currentThread();
+    timer.stop();
+    loop.quit();
+    qInfo() << this <<"stopperd on "<<QThread::currentThread();
+}
+
+void Worker::timeout()
+{
+    qInfo() << this << "Timeout on " << QThread::currentThread();
+}
